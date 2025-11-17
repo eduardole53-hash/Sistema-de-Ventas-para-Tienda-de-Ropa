3d0406f9ba8e0de1610ba985f4d98096fe32300c
Domingo Deleon <eduardo.le53@gmail.com>
2025-11-16 21:56:57 -0500

    users.js

--- backend/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-// src/routes/users.js
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/users.controller");
-
-router.post("/register", userController.registrarUsuario);
-router.post("/login", userController.loginUsuario);
-
-module.exports = router;
